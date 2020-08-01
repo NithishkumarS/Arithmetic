6964346d1a9c089cb358d3d7c23f9f5fc484e4e0
nithishkumarS <nithishtimes@gmail.com>
2020-08-01 12:49:42 -0400

    main files to run the operations

---
+++ src/main.cpp
@@ -0,0 +1,35 @@
+/*
+ * main.cpp
+ *
+ *  Created on: July 31, 2020
+ *      Author: Nithish
+ */
+
+#include <iostream>
+#include <Multiplier.h>
+#include <string>
+#include "Factory.h"
+#include "Arithmetic.h"
+#include "utils.h"
+
+
+int main(int argc, char* argv[]){
+
+
+
+  std::string engine;
+  engine = argv[1];
+
+  std::vector<double> elements = parseInput(argc, argv);
+//  std::vector<int> elements;
+  if(!elements.empty()) {
+
+	  Arithmetic<double> *engine_ptr = Arithmetic<double>::factory(engine);
+
+	  double output = engine_ptr->operation(elements);
+	  std::cout<<output<<std::endl;
+  }
+
+  return 0;
+
+}
