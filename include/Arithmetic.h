cdda188747957cb4fb96f6183fb4416392da90b5
nithishkumarS <nithishtimes@gmail.com>
2020-08-01 12:11:02 -0400

    Added parent engine class

---
+++ include/Arithmetic.h
@@ -0,0 +1,40 @@
+/*
+ * Arithmetic.h
+ *
+ *  Created on: July 31, 2020
+ *      Author: Nithish
+ */
+
+#ifndef INCLUDE_ARITHMETIC_H_
+#define INCLUDE_ARITHMETIC_H_
+
+#include <iostream>
+#include <vector>
+
+template <typename T>
+
+class Arithmetic {
+
+public:
+	Arithmetic() {};
+
+	virtual ~Arithmetic() {};
+
+	/**
+	 * Function to create an engine object from the input
+	 * params :  name of engine
+	 * return :  engine object
+	 */
+	static Arithmetic *factory(std::string& engine);
+
+	/*
+	 * Pure virtual function to that has to redefined for the corresponding arithmetic operation
+	 * params :  vector of operands
+	 * return :  Computed engine output
+	 */
+	virtual double operation(std::vector<T>& elements) = 0;
+};
+
+
+
+#endif /* INCLUDE_ARITHMETIC_H_ */
